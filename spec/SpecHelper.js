cf2a4ce29223d399f8beb938acd2384782488a18
7696122 <7696122@gmail.com>
2021-09-24 19:01:37 +0900

    feat: Add custom matcher

--- spec/SpecHelper.js
+++ spec/SpecHelper.js
@@ -1,15 +1,25 @@
 beforeEach(function () {
   jasmine.addMatchers({
+    toBeAGoodInvestment: function () {
+      return {
+        compare: function (actual, expected) {
+          const investment = actual;
+          return {
+            pass: investment.isGood(),
+          };
+        },
+      };
+    },
     toBePlaying: function () {
       return {
         compare: function (actual, expected) {
           var player = actual;
 
           return {
-            pass: player.currentlyPlayingSong === expected && player.isPlaying
+            pass: player.currentlyPlayingSong === expected && player.isPlaying,
           };
-        }
+        },
       };
-    }
+    },
   });
 });
