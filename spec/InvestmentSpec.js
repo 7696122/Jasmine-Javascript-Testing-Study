5af5f712dc5c5669ec78ecc4a5eaeb24d23ee033
7696122 <7696122@gmail.com>
2021-09-27 12:20:40 +0900

    up

--- spec/InvestmentSpec.js
+++ spec/InvestmentSpec.js
@@ -28,6 +28,16 @@ describe("주식 투자는", () => {
     });
   });
 
+  describe("주가가 하락하면", () => {
+    beforeEach(() => {
+      stock.sharePrice = 0;
+    });
+
+    it("불량 투자다", () => {
+      expect(investment).not.toBeAGoodInvestment();
+    });
+  });
+
   it("대상이 주식이어야 한다", () => {
     expect(investment.stock).toBe(stock);
   });
@@ -42,5 +52,109 @@ describe("주식 투자는", () => {
 
   it("비용이 수반된다", () => {
     expect(investment.cost).toEqual(2000);
+  });
+});
+
+describe("toEqual 테스트", () => {
+  it("동등한 숫자다", function () {
+    expect(1).toEqual(1);
+  });
+  it("동등한 문자열이다", function () {
+    expect("testing").toEqual("testing");
+  });
+  it("동등한 객체이다", function () {
+    expect({ a: "testing" }).toEqual({ a: "testing" });
+  });
+  it("동등한 배열이다", function () {
+    expect([1, 2, 3]).toEqual([1, 2, 3]);
+  });
+});
+
+describe("toBe 테스트", () => {
+  it("동등한 숫자다", function () {
+    expect(1).toBe(1);
+  });
+  it("동등한 문자열이다", function () {
+    expect("테스팅").toBe("테스팅");
+  });
+  it("동등한 불린값이다", function () {
+    expect(true).toBe(true);
+  });
+  it("동등한 객체다", function () {
+    const obj = { a: "testing" };
+    expect(obj).toBe(obj);
+  });
+  it("등등한 배열이다", function () {
+    const arr = [1, 2, 3];
+    expect(arr).toBe(arr);
+  });
+  it("등등한 객체가 아니다", function () {
+    const obj = { a: "testing" };
+    expect(obj).not.toBe({ a: "testing" });
+  });
+  it("동등한 배열이 아니다", function () {
+    expect([1, 2, 3]).not.toBe([1, 2, 3]);
+  });
+});
+
+describe("toBeFalsy테스트", () => {
+  it("undefined는 falsy다", function () {
+    expect(undefined).toBeFalsy();
+  });
+  it("null은 falsy다", function () {
+    expect(null).toBeFalsy();
+  });
+  it("NaN은 falsy다", function () {
+    expect(NaN).toBeFalsy();
+  });
+  it("false는 falsy다", function () {
+    expect(false).toBeFalsy();
+  });
+  it("should be 0 is falsy", function () {
+    expect(false).toBeFalsy();
+  });
+  it("should be empty string is falsy", function () {
+    expect("").toBeFalsy();
+  });
+});
+
+// 그러나 정확한 불린값으로 비교하고자 한다면 toEqual 매처를 사용하는 것이 바람직하다
+describe("Test truthy", () => {
+  it("should be true is truthy", function () {
+    expect(true).toBeTruthy();
+  });
+  it("0이 아닌 숫자는 truthy다", function () {
+    expect(1).toBeTruthy();
+  });
+  it("should be non-empty string is truthy", function () {
+    expect("a").toBeTruthy();
+  });
+  it("should be array and object is truthy", function () {
+    expect([]).toBeTruthy();
+    expect({}).toBeTruthy();
+  });
+});
+
+describe("Test toBeNull", () => {
+  it("should be null is null", function () {
+    expect(null).toBeNull();
+  });
+});
+
+describe("Test toBeUndefined", function () {
+  it("should be undefined is undefined", function () {
+    expect(undefined).toBeUndefined(undefined);
+  });
+});
+
+describe("Test toBeNaN", function () {
+  it("should be NaN is NaN", function () {
+    expect(NaN).toBeNaN();
+  });
+});
+
+describe("toMatch 테스트", () => {
+  it("문자열이 정규식에 매칭된다", function () {
+    expect("My big matched string").toMatch(/My(.+)string/);
   });
 });
--- spec/SpecHelper.js
+++ spec/SpecHelper.js
@@ -1,11 +1,30 @@
 beforeEach(function () {
+  // jasmine.expect.expand({
+  //   toBeABedInvestment(received) {
+  //     const pass = received.isGood();
+  //     if (pass) {
+  //       return {
+  //         pass: true,
+  //         message: ``,
+  //       };
+  //     } else {
+  //       return {
+  //         pass: false,
+  //         message: ``,
+  //       };
+  //     }
+  //   },
+  // });
+
   jasmine.addMatchers({
     toBeAGoodInvestment: function () {
       return {
-        compare: function (actual, expected) {
+        compare: function (actual, isNot) {
           const investment = actual;
+          let what = isNot ? "bad" : "good";
           return {
             pass: investment.isGood(),
+            message: `Expected investment to be a ${what} investment`,
           };
         },
       };
